0ef16c7e6ac8cc2132942d14f92f4e53ce81a226
Yuri Mikushov <mikushov.yu@gmail.com>
2021-01-06 18:33:24 +0300

    feature: added dark theme

--- src/components/DarkModeButton.vue
+++ src/components/DarkModeButton.vue
@@ -1,25 +1,49 @@
 <template>
-  <button class="dark-mode-button" @click="toggle">
-    {{ theme.name === THEME_NAME.DARK ? '☀️' : '🌙' }}
+  <button class="dark-mode-button" @click="toggleTheme">
+    <span v-if="isLightTheme">🌙</span>
+    <span v-else>☀️</span>
   </button>
 </template>
 
 <script>
+const THEME_NAME = {
+  LIGHT: 'light',
+  DARK: 'dark',
+}
+
 export default {
   name: 'DarkModeButton',
   data() {
     return {
-      theme: {
-        name: 'light',
-      },
-      THEME_NAME: {
-        LIGHT: 'light',
-        DARK: 'dark',
-      },
+      themeName: THEME_NAME.LIGHT,
     }
   },
+  computed: {
+    isLightTheme() {
+      return this.themeName === THEME_NAME.LIGHT
+    },
+  },
   methods: {
-    toggle: () => console.log('toggle theme'),
+    toggleTheme: function () {
+      this.themeName =
+        this.themeName === THEME_NAME.LIGHT ? THEME_NAME.DARK : THEME_NAME.LIGHT
+
+      this.setThemeNameInHtmlDataset()
+      this.setThemeNameInLocalStorage()
+    },
+    setThemeNameInHtmlDataset: function () {
+      document.querySelector('html').dataset['theme'] = this.themeName
+    },
+    setThemeNameInLocalStorage: function () {
+      localStorage.setItem('themeName', this.themeName)
+    },
+    getThemeNameFromLocalStorage: function () {
+      return localStorage.getItem('themeName')
+    },
+  },
+  mounted() {
+    this.themeName = this.getThemeNameFromLocalStorage() || THEME_NAME.LIGHT
+    this.setThemeNameInHtmlDataset()
   },
 }
 </script>
@@ -30,5 +54,24 @@ export default {
   background-color: transparent;
   font-size: 0.75rem;
   outline: 0;
+}
+
+html[data-theme='light'] {
+  --bg-color: #f4f4f4;
+  --calc-bg-color: #000;
+  --calc-screen-bg-color: #fff;
+  --top-operator-button-bg-color: #f8f8f8;
+  --num-button-bg-color: #fff;
+  --right-side-operator-button-bg-color: #f27200;
+  --font-color: #000;
+}
+
+html[data-theme='dark'] {
+  --bg-color: #1f2022;
+  --calc-bg-color: #fff;
+  --calc-screen-bg-color: #262626;
+  --top-operator-button-bg-color: #424242;
+  --num-button-bg-color: #262626;
+  --font-color: #fff;
 }
 </style>
